544632d87d09d7417b26124ab59a89995859c93d
EvgenyFA <31123205+EvgenyFA@users.noreply.github.com>
2017-09-27 00:01:51 +0700

    Add files via upload

---
+++ task2_bcs/task2_bcs/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task2_bcs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadFiles"), filePath));
+            }
+
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = Path.GetFullPath(Server.MapPath("~/UploadFiles/unpackzip.bat"));
+                process.StartInfo.WorkingDirectory = Path.GetFullPath(Server.MapPath("~/UploadFiles"));
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                process.Start();
+            }
+            catch
+            {
+                return Json("file uploaded successfully");
+            }
+
+            return Json("file uploaded successfully");
+        }
+    }
+}
